c956e9fd53d9e0b19b77b8a81a975b7459981c88
Jesus <jcardenasv@cuc.edu.co>
2026-05-26 12:08:44 -0500

    Ahora los logos están en public, no en storage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') - Pilla Tu Cancha</title>
-    <link rel="icon" type="image/png" href="{{ asset('storage/logo/favicon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('logo/favicon.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -13,7 +13,7 @@
         <div class="container-fluid">
             
             <a class="navbar-brand d-flex align-items-center" href="{{ route('home') }}" style="position: relative;">
-                <img src="{{ asset('storage/logo/logo.png') }}" 
+                <img src="{{ asset('logo/logo.png') }}" 
                     alt="Pilla Tu Cancha" 
                     width="48" 
                     height="48" 
